d3bc4284e723102417395882cc566761cb6312b5
StardustYuri <sean20031229@gmail.com>
2024-05-14 14:02:33 -0500

    funny

---
+++ Cirby/Assets/Player.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public float speed;
+    public float jumpAmount;
+    float inputMovement;
+
+    Rigidbody2D rb;
+
+    bool touchesGround;
+    public Transform groundChecker;
+    public LayerMask groundMask;
+
+    public float radius;
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    private void FixedUpdate()
+    {
+        touchesGround = Physics2D.OverlapCircle(groundChecker.position, radius, groundMask);
+
+        inputMovement = Input.GetAxis("Horizontal");
+        rb.velocity = new Vector2(inputMovement * speed, rb.velocity.y);
+    }
+
+    private void Update()
+    {
+        if(touchesGround && Input.GetKeyDown(KeyCode.Space))
+        {
+            rb.velocity = Vector2.up * jumpAmount;
+        }
+    }
+}
