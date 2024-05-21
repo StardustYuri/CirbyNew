87513ed9348dc062335bcf96203e89a94f930333
Boomerdoom <123778221+Boomerdoom@users.noreply.github.com>
2024-05-21 15:15:55 -0500

    hello

--- Cirby/Assets/Bullet Collision.cs
+++ Cirby/Assets/Bullet Collision.cs
@@ -8,27 +8,30 @@ public class BulletCollision : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
     }
     void OnCollisionEnter2D(Collision2D collision)
     {
-        
+
 
         if (collision.gameObject.tag != "Ground") //&& collision.gameObject.tag != "Player")
         {
             Destroy(collision.gameObject);
             Debug.Log("Object Deleted (ground code)");
         }
-       /* if (collision.gameObject.tag != "Player")
-        {
-            Destroy(gameObject);
-            Debug.Log("Object Deleted");
-        }
-   */ }
+        Destroy(gameObject);
+        /* if (collision.gameObject.tag != "Player")
+         {
+             Destroy(gameObject);
+             Debug.Log("Object Deleted");
+         }
+    */
+    }
 }
+
